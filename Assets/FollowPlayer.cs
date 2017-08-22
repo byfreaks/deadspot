12d9fe60e795d1022168af2a4fcfd7937b95d8c5
zchang <zchangvx@hotmail.com>
2017-08-21 23:31:45 -0400

    Fundamentos del movimiento de Kira

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null) 
+			this.transform.position = new Vector3(player.transform.position.x,this.transform.position.y,-10);
+		
+	}
+}
---
+++ Assets/Scripts/Player Scripts/MainScriptPlayer.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScriptPlayer : MonoBehaviour {
+
+	//Sprites
+	public Sprite sprAimingKira;
+	public Sprite sprNormalKira;
+
+	//Components
+	private Rigidbody2D rb;
+	private BoxCollider2D boxColl;
+
+	//Body parts
+	[Header("Body parts")]
+	public GameObject playerTorso;
+	public GameObject playerLegs;
+
+	//Design Public
+	[Header("Physics settings")]
+	public float moveSpeed = 3;
+	public float moveAim = 100;
+	public float jumpForce = 3;
+
+	//Other
+	private string toDisplay;
+
+	//Private
+	private bool facingRight = true;
+	private bool isAiming = false;
+	private float spd = 0;
+
+
+	void Start () {
+		//Get components
+		rb = GetComponent<Rigidbody2D>();
+		boxColl = GetComponent<BoxCollider2D>();
+
+		rb.freezeRotation = true;
+	}
+	
+	void Update () {
+
+		if (Input.GetMouseButton(1)){
+			toDisplay = "State: Aiming";
+			isAiming = true;
+		} else {
+			toDisplay = "State: Normal";
+			isAiming = false;
+		}
+
+		//Movement
+		if (isAiming) spd = moveSpeed - moveAim;
+		else spd = moveSpeed;
+
+		if (Input.GetKey(KeyCode.D)){
+			rb.velocity = new Vector2( spd, rb.velocity.y);
+			facingRight = true;
+		}
+		if (Input.GetKey(KeyCode.A)){
+			rb.velocity = new Vector2(-spd, rb.velocity.y);
+			facingRight = false;
+		}
+		if (Input.GetKeyDown(KeyCode.Space)){
+			rb.velocity = new Vector2(rb.velocity.x, jumpForce );
+		}
+
+		GameObject.Find("Text").GetComponent<Text>().text = toDisplay;	
+		
+		//Animation
+		
+		
+
+		if (isAiming){
+			playerTorso.GetComponent<SpriteRenderer>().sprite = sprAimingKira;
+
+			//TODO declare proper vars
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        	playerTorso.transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position  );
+
+			if (mousePos.x > transform.position.x) facingRight = true;
+			else facingRight = false;
+
+		} else {
+			playerTorso.GetComponent<SpriteRenderer>().sprite = sprNormalKira;
+			playerTorso.transform.localEulerAngles = new Vector3(0, 0, 0);
+
+		}
+
+		playerTorso.GetComponent<SpriteRenderer>().flipX = !facingRight;
+		playerLegs.GetComponent<SpriteRenderer>().flipX = !facingRight;
+		
+	}
+}
